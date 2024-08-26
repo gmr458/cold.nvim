519391009cd8648e0377cbc86629cc611598029f
German David <germanmarinrolong@gmail.com>
2024-08-26 12:39:09 -0500

    ColdColorschemeFile command added

--- lua/cold/init.lua
+++ lua/cold/init.lua
@@ -29,14 +29,10 @@ M.config = {
     custom_statusline_dark_background = nil,
 }
 
---- @overload fun(config?: ColdConfig)
-function M.setup(config)
-    M.config = vim.tbl_deep_extend('force', M.config, config or {})
-end
-
 --- @param theme_dark ColdThemeDark
 --- @param theme_light ColdThemeLight
-function M.compile(theme_dark, theme_light)
+--- @return string
+local function generate_lines(theme_dark, theme_light)
     local lines = {
         string.format [[return string.dump(function()
 vim.cmd.highlight('clear')
@@ -75,13 +71,25 @@ vim.opt.termguicolors=true]],
 
     table.insert(lines, 'end,true)')
 
+    return table.concat(lines, '\n')
+end
+
+--- @overload fun(config?: ColdConfig)
+function M.setup(config)
+    M.config = vim.tbl_deep_extend('force', M.config, config or {})
+end
+
+--- @param theme_dark ColdThemeDark
+--- @param theme_light ColdThemeLight
+function M.compile(theme_dark, theme_light)
     local cold_cache_dir = vim.fn.stdpath 'cache' .. '/cold/'
 
     if vim.fn.isdirectory(cold_cache_dir) == 0 then
         vim.fn.mkdir(cold_cache_dir, 'p')
     end
 
-    local f = loadstring(table.concat(lines, '\n'))
+    local concataned_lines = generate_lines(theme_dark, theme_light)
+    local f = loadstring(concataned_lines)
     if not f then
         local path_debug_file = vim.fn.stdpath 'state' .. '/cold-debug.lua'
 
@@ -93,7 +101,7 @@ vim.opt.termguicolors=true]],
 
         local debug_file = io.open(path_debug_file, 'wb')
         if debug_file then
-            debug_file:write(table.concat(lines, '\n'))
+            debug_file:write(concataned_lines)
             debug_file:close()
         end
         return
@@ -148,6 +156,42 @@ vim.api.nvim_create_user_command('ColdCompile', function()
 
     vim.notify('[cold.nvim] colorscheme compiled', vim.log.levels.INFO)
     vim.cmd.colorscheme 'cold'
+end, {})
+
+--- @param theme_dark ColdThemeDark
+--- @param theme_light ColdThemeLight
+--- @param path string
+function M.generate_colorscheme_file(theme_dark, theme_light, path)
+    local concataned_lines = generate_lines(theme_dark, theme_light)
+
+    local file = io.open(path, 'wb')
+    if file then
+        file:write(concataned_lines)
+        file:close()
+    else
+        vim.notify(
+            '[cold.nvim] error trying to generate colorscheme file',
+            vim.log.levels.ERROR
+        )
+    end
+end
+
+vim.api.nvim_create_user_command('ColdColorschemeFile', function()
+    local palette = require 'cold.palette'
+
+    local theme_dark = require('cold.themes').dark(palette, M.config)
+    local theme_light = require('cold.themes').light(palette, M.config)
+
+    local cold_state_dir = vim.fn.stdpath 'state' .. '/cold'
+    if vim.fn.isdirectory(cold_state_dir) == 0 then
+        vim.fn.mkdir(cold_state_dir, 'p')
+    end
+
+    local path = cold_state_dir .. '/cold.lua'
+    M.generate_colorscheme_file(theme_dark, theme_light, path)
+
+    local msg = string.format('[cold.nvim] colorscheme file generated at %s', path)
+    vim.notify(msg, vim.log.levels.INFO)
 end, {})
 
 return M
